fffcc7f471a4e2d4ca3b6d8c43a77a3dcb6f0d90
objektwerks <objektwerks@gmail.com>
2015-12-29 11:35:35 -0500

    returned to kafka 0.9.0.0; refactoring test

--- src/test/scala/kafka/KafkaTest.scala
+++ src/test/scala/kafka/KafkaTest.scala
@@ -2,18 +2,16 @@ package kafka
 
 import java.util.Properties
 
-import kafka.admin.AdminUtils
 import kafka.producer.{KeyedMessage, Producer, ProducerConfig}
-import kafka.utils.ZKStringSerializer
-import org.I0Itec.zkclient.ZkClient
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.scalatest.FunSuite
 
+import scala.collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 class KafkaTest extends FunSuite {
-  val kafkaTopic = "ratings"
+  val kafkaTopic = "kv"
 
   test("kafka") {
     createKafkaTopic()
@@ -22,41 +20,29 @@ class KafkaTest extends FunSuite {
   }
 
   private def createKafkaTopic(): Unit = {
-    val zkClient = new ZkClient("localhost:2181", 3000, 3000, ZKStringSerializer)
-    val metadata = AdminUtils.fetchTopicMetadataFromZk(kafkaTopic, zkClient)
-    metadata.partitionsMetadata.foreach(println)
-    if (metadata.topic != kafkaTopic) {
-      AdminUtils.createTopic(zkClient, kafkaTopic, 1, 1)
-    }
+
   }
 
-  private def produceAndSendKafkaTopicMessages(): Seq[(String, String, String, String)] = {
-    val props = new Properties
-    props.load(Source.fromInputStream(getClass.getResourceAsStream("/kafka.properties")).bufferedReader())
-    val config = new ProducerConfig(props)
+  private def produceAndSendKafkaTopicMessages(): Unit = {
+    val config = new ProducerConfig(loadProperties("/kafka.producer.properties"))
     val producer = new Producer[String, String](config)
-    val source = Source.fromInputStream(getClass.getResourceAsStream("/ratings")).getLines.toSeq
-    val ratings = ArrayBuffer[(String, String, String, String)]()
     val keyedMessages = ArrayBuffer[KeyedMessage[String, String]]()
-    source foreach { line =>
-      val fields = line.split(",")
-      val rating = (fields(0), fields(1), fields(2), fields(3))
-      ratings += rating
-      keyedMessages += KeyedMessage[String, String](topic = kafkaTopic, key = line, partKey = 0, message = line)
+    for (i <- 1 to 10) {
+      val keyValue = i.toString
+      keyedMessages += KeyedMessage[String, String](topic = kafkaTopic, key = keyValue, partKey = 0, message = keyValue)
     }
     producer.send(keyedMessages: _*)
-    ratings
+    producer.close()
   }
 
   private def consumeKafkaTopicMessages(): Unit = {
-    val props = new Properties
-    props.load(Source.fromInputStream(getClass.getResourceAsStream("/kafka.properties")).bufferedReader())
-    val consumer = new KafkaConsumer(props)
-    consumer.subscribe(kafkaTopic)
-    val isRunning = true
-    while(isRunning) {
-      consumer.poll(100)
-    }
-    consumer.close()
+    val consumer = new KafkaConsumer[String, String](loadProperties("/kafka.consumer.properties"))
+    consumer.subscribe(List(kafkaTopic).asJava)
+  }
+
+  private def loadProperties(file: String): Properties = {
+    val properties = new Properties()
+    properties.load(Source.fromInputStream(getClass.getResourceAsStream(file)).bufferedReader())
+    properties
   }
 }

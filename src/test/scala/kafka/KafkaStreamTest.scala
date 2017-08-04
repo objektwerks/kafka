9b50a558a8b202b3f6830f7cd2bdf69f9c8bab1d
objektwerks <objektwerks@runbox.com>
2017-08-04 11:10:45 -0400

    added stream producer to kafka stream test

--- src/test/scala/kafka/KafkaStreamTest.scala
+++ src/test/scala/kafka/KafkaStreamTest.scala
@@ -1,5 +1,8 @@
 package kafka
 
+import java.util.concurrent.TimeUnit
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.log4j.Logger
 import org.scalatest.{FunSuite, Matchers}
 
@@ -7,6 +10,15 @@ class KafkaStreamTest extends FunSuite with Matchers {
   val logger = Logger.getLogger(classOf[KafkaStreamTest])
 
   test("kafka stream") {
+    val topic = TestConfig.valueKafkaTopic
+    TestConfig.assertTopic(topic) shouldBe topic
+    produceStream(topic, TestConfig.gettysburgAddress)
+  }
 
+  def produceStream(topic: String, words: Seq[String]): Unit = {
+    val producer = new KafkaProducer[String, String](TestConfig.kafkaProducerProperties)
+    words foreach { word => producer.send(new ProducerRecord[String, String](topic, word)) }
+    producer.flush()
+    producer.close(1000L, TimeUnit.MILLISECONDS)
   }
 }

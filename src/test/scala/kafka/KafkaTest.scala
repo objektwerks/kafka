912123b0bf01ea93b63b6be80c000217456e88fb
objektwerks <objektwerks@runbox.com>
2019-07-22 20:12:51 -0400

    implemented tx producer / consumer tests; more yet to do, though

--- src/test/scala/kafka/KafkaTest.scala
+++ src/test/scala/kafka/KafkaTest.scala
@@ -4,6 +4,7 @@ import java.time.Duration
 
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.common.KafkaException
 import org.scalatest.{FunSuite, Matchers}
 import org.slf4j.LoggerFactory
 
@@ -38,12 +39,12 @@ class KafkaTest extends FunSuite with Matchers {
     consumeTxMessages(topic)
     val postConsumeTxMessageCount = countMessages(topic)
 
-    postProduceTxMessageCount should be >= 0 // TODO
+    postProduceTxMessageCount should be >= 3
     postConsumeTxMessageCount shouldEqual 0
   }
 
   def produceMessages(topic: String, count: Int): Unit = {
-    val producer = new KafkaProducer[String, String](KafkaCommon.kafkaProducerProperties)
+    val producer = new KafkaProducer[String, String](kafkaProducerProperties)
     for (i <- 1 to count) {
       val key = i.toString
       val value = key
@@ -57,7 +58,7 @@ class KafkaTest extends FunSuite with Matchers {
   }
 
   def consumeMessages(topic: String): Unit = {
-    val consumer = new KafkaConsumer[String, String](KafkaCommon.kafkaConsumerProperties)
+    val consumer = new KafkaConsumer[String, String](kafkaConsumerProperties)
     consumer.subscribe(List(topic).asJava)
     for (i <- 1 to 2) {
       val records = consumer.poll(Duration.ofMillis(100L))
@@ -65,16 +66,46 @@ class KafkaTest extends FunSuite with Matchers {
       records.iterator.asScala.foreach { record =>
         logger.info(s"*** Consumer -> topic: ${record.topic} partition: ${record.partition} offset: ${record.offset} key: ${record.key} value: ${record.value}")
       }
-      if (records.count > 0) consumer.commitAsync()
+      if (records.count > 0) consumer.commitSync()
     }
     consumer.close()
   }
 
   def produceTxMessages(topic: String, count: Int): Unit = {
-    ()
+    val producer = new KafkaProducer[String, String](kafkaProducerTxProperties)
+    producer.initTransactions()
+    try {
+      producer.beginTransaction()
+      for (i <- 1 to count) {
+        val key = i.toString
+        val value = key
+        val record = new ProducerRecord[String, String](topic, key, value)
+        val metadata = producer.send(record).get()
+        logger.info(s"*** Tx Producer -> topic: ${metadata.topic} partition: ${metadata.partition} offset: ${metadata.offset}")
+        logger.info(s"*** Tx Producer -> key: ${record.key} value: ${record.value}")
+      }
+      producer.commitTransaction()
+    } catch {
+      case error:KafkaException =>
+        logger.error(s"*** Tx Producer error: $error")
+        producer.abortTransaction()
+    } finally {
+      producer.flush()
+      producer.close()
+    }
   }
 
   def consumeTxMessages(topic: String): Unit = {
-    ()
+    val consumer = new KafkaConsumer[String, String](kafkaConsumerTxProperties)
+    consumer.subscribe(List(topic).asJava)
+    for (i <- 1 to 2) {
+      val records = consumer.poll(Duration.ofMillis(100L))
+      logger.info(s"*** Tx Consumer -> { ${records.count} } records polled on attempt { $i }.")
+      records.iterator.asScala.foreach { record =>
+        logger.info(s"*** Tx Consumer -> topic: ${record.topic} partition: ${record.partition} offset: ${record.offset} key: ${record.key} value: ${record.value}")
+      }
+      if (records.count > 0) consumer.commitAsync()
+    }
+    consumer.close()
   }
 }
